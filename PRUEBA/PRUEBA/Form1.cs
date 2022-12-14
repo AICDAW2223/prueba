b9bd270b45202437a2b36d1333985163963b08ab
AICDAW2223 <adribacan@alu.edu.gva.es>
2022-12-15 00:27:16 +0100

    Prerama

--- PRUEBA/PRUEBA/Form1.cs
+++ PRUEBA/PRUEBA/Form1.cs
@@ -16,5 +16,10 @@ namespace PRUEBA
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1;
+        }
     }
 }
